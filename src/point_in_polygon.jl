964d798e90d1e15e3e51c4d0d8b6217ecdeed7e7
Lior Sinai <sinailior@gmail.com>
2025-01-04 17:46:30 +0200

    fix: point in polygon -> typo + repeated points. - Vertix -> vertex - Repeated points through the num_intersections count off, so skip them. - Make the extremes less extreme, to make the rtol more likely. Actually, these only need to be slightly bigger than the min and max. Even +-1 would suffice. - avoid the implicit type change by making intersect a boolean, until the addition

--- src/point_in_polygon.jl
+++ src/point_in_polygon.jl
@@ -7,7 +7,7 @@ This algorithm is an an extension of the odd-even ray algorithm.
 It is based on "A Simple and Correct Even-Odd Algorithm for the Point-in-Polygon Problem for Complex Polygons" 
 by Michael Galetzka and Patrick Glauner (2017). 
 It skips vertices that are on the ray. To compensate, the ray is projected backwards (to the left) so that an 
-intersection can be found for a skipped vertix if needed.
+intersection can be found for a skipped vertex if needed.
 
 `rtol` should be small because of the extreme points used in the algorithm.
 """
@@ -16,8 +16,8 @@ function contains(vertices::Polygon2D, point::Point2D{T}; on_border_is_inside::B
     num_intersections = 0
 
     x = x_coords(vertices)
-    extreme_left =  (minimum(x) - T(1e6), point[2])
-    extreme_right = (maximum(x) + T(1e6), point[2])
+    extreme_left =  (minimum(x) - T(1e3), point[2])
+    extreme_right = (maximum(x) + T(1e3), point[2])
 
     # step 1: point intersects a vertex or edge
     for i in 1:n
@@ -31,26 +31,27 @@ function contains(vertices::Polygon2D, point::Point2D{T}; on_border_is_inside::B
     # step 3: check intersections with vertices
     s = 1
     while s <= n
-        # step 2: find a vertix not on the same horizontal ray as the point
+        # step 2: find a vertex not on the same horizontal ray as the point
         while (s <= n) && (vertices[s][2] == point[2])
             s += 1
         end
         if s > n
             break
         end
-        # step 3a: find the next vertix not on the horizontal ray
+        # step 3a: find the next vertex not on the horizontal ray
         next_s = s
         skipped_right = false
         for i in 0:n
             next_s = (next_s) % n + 1
-            if vertices[next_s][2] != point[2]
+            if (vertices[next_s][2] != point[2]) && 
+                (vertices[s] != vertices[next_s]) # TODO tolerance
                 break
             end
             skipped_right = skipped_right || (vertices[next_s][1] > point[1])
         end
         # step 3b: edge intersect with the ray
         edge = (vertices[s], vertices[next_s])
-        intersect = 0
+        intersect = false
         if (next_s - s) == 1 || (s == n && next_s ==1) # 3b.i
             intersect = do_intersect(edge, (point, extreme_right); rtol=rtol)
         elseif skipped_right # 3b.ii
